65262b4b5ae1bc4c22794c4cb09eecf656cc30e2
lokesh2646 <93193473+lokesh2646@users.noreply.github.com>
2022-01-28 20:31:56 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,13 @@
+
+public class loops{
+
+    public static void main(String args[]){
+        int i;
+        for(i=0;i<=20;i++)
+        {
+           System.out.println(i);
+        }
+
+
+    }
+}

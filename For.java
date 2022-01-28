5eab707a11399168a7ddeacbc79e3df1ec894c14
lokesh2646 <93193473+lokesh2646@users.noreply.github.com>
2022-01-28 20:35:26 +0530

    Add files via upload

---
+++ For.java
@@ -0,0 +1,12 @@
+
+public class For{
+
+    public static void main(String args[]){
+        int i;
+        for(i=0;i<=20;i++)
+        {
+           System.out.println("java programming");
+        }
+
+    }
+}

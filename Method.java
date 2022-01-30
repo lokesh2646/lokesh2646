dea34212c4718df126489c8e2c98ff392f57e7fb
lokesh2646 <93193473+lokesh2646@users.noreply.github.com>
2022-01-30 23:14:56 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,15 @@
+
+public class Method{
+    void add()
+    {
+        int a=14;
+        int b=35;
+        int c =a+b;
+        System.out.println(c);
+    }
+
+    public static void main(String args[])   {
+        Method me=new Method();
+         me.add();
+    }
+}

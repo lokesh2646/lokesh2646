04cf9bb3b1fa8eeb6f6be8f37bac3298e7a4ee05
lokesh2646 <93193473+lokesh2646@users.noreply.github.com>
2022-02-01 00:15:40 +0530

    Add files via upload

---
+++ IfElseAndCompleteLoops.java
@@ -0,0 +1,37 @@
+
+public class IfElseAndCompleteLoops{
+    public static void main(String args[]){
+         int i; 
+         for(i=8;i<=6;i++) {
+              System.out.println(i +" JAVA IS A POWER-PACKED PROGRAMMING LANGUAGE");
+              System.out.println("FOR LOOP MAIN");
+               } 
+     if(i<=5) 
+               {
+      System.out.println("OOPS"); System.out.println("EXCEPTION HANDLING"); System.out.println("USER DEFINED PACKAGES");               System.out.println("THREADS"); System.out.println("MEMORY MANAGEMENT");
+      System.out.println("IF BLOCK IN NORMAL FOR LOOP");
+       }
+       else
+{
+       int t=10;
+       while(t<=15)
+       {
+           System.out.println("While block "+t);
+           t++;
+       }
+       int h=28;
+       do
+       {
+           System.out.println("dO While bolck "+h);
+           h++;
+       }while(h<=30);
+
+       int g;
+       for(g=9;g<=10;g++)
+       {
+           System.out.println("Else For Loop " +g);
+       }
+       System.out.println("JAVA  ELSE   BLOCK");
+       }
+    }
+ }
